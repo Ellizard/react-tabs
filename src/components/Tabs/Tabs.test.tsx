49dd116309974671e585b53fe130581d8b03df09
Artem Ostavnenko <artem.ostavnenko@danfoss.com>
2023-02-28 10:26:15 +0200

    Test remove

--- src/components/Tabs/Tabs.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-
-describe('another suite', () => {
-  it('skipped test', () => {
-    // Test skipped, as tests are running in Only mode
-    expect(Math.sqrt(4)).toEqual(2);
-  });
-});
